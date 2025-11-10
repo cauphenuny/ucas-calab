d6f874daeb8bf02e511cd3266dbf53afe0a322e0
cauphenuny <starseekist@gmail.com>
2025-11-10 12:47:25 +0800

    exp13: chore: update tcl files

--- exp13/soc_verify/soc_bram/run_vivado/create_project.tcl
+++ exp13/soc_verify/soc_bram/run_vivado/create_project.tcl
@@ -7,6 +7,37 @@ add_files -scan_for_includes [glob -nocomplain ../rtl/*/*.v]
 # Add IPs
 add_files -quiet [glob -nocomplain ../rtl/xilinx_ip/*/*.xci]
 
+# =========================================================
+# 自动创建有符号/无符号除法器 (Vivado 2023.2 div_gen v5.1)
+# =========================================================
+
+# 创建无符号除法器
+create_ip -name div_gen -vendor xilinx.com -library ip -version 5.1 -module_name udiv
+set_property -dict [list \
+    CONFIG.algorithm_type {Radix2} \
+    CONFIG.operand_sign {Unsigned} \
+    CONFIG.dividend_and_quotient_width {32} \
+    CONFIG.divisor_width {32} \
+    CONFIG.remainder_type {Remainder} \
+    CONFIG.clocks_per_division {8} \
+    CONFIG.FlowControl {NonBlocking} \
+] [get_ips udiv]
+generate_target all [get_ips udiv]
+
+# 创建有符号除法器
+create_ip -name div_gen -vendor xilinx.com -library ip -version 5.1 -module_name sdiv
+set_property -dict [list \
+    CONFIG.algorithm_type {Radix2} \
+    CONFIG.operand_sign {Signed} \
+    CONFIG.dividend_and_quotient_width {32} \
+    CONFIG.divisor_width {32} \
+    CONFIG.remainder_type {Remainder} \
+    CONFIG.clocks_per_division {8} \
+    CONFIG.FlowControl {NonBlocking} \
+] [get_ips sdiv]
+generate_target all [get_ips sdiv]
+
+
 # Add simulation files
 add_files -fileset sim_1 ../testbench/mycpu_tb.v
 
