e06cf1e4553cb0966ea99913f16a95f8a9f88dea
cauphenuny <starseekist@gmail.com>
2025-11-24 17:28:14 +0800

    exp14: chore: add lsp&linter files

---
+++ exp14/.lazy.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    -- https://github.com/mfussenegger/nvim-lint
+    "mfussenegger/nvim-lint",
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = {
+      linters_by_ft = {
+        verilog = { "verilator" },
+      },
+    },
+    init = function()
+      local verilator = require("lint").linters.verilator
+      verilator.args = {
+        "-Wall",
+        "--bbox-sys",
+        "--bbox-unsup",
+        "--lint-only",
+        "-ImyCPU",
+        "--top-module", "mycpu_top",
+      }
+    end,
+  },
+
+}
